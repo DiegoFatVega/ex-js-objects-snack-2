5ae16508152e8eaa05dd1af665c30379f2fe04d0
DiegoFatVega <fatvegadiego@gmail.com>
2025-12-09 21:33:22 +0100

    add code snippets for questions 3 and 4 in ex-js-objects-snack-2.js

--- ex-js-objects-snack-2.js
+++ ex-js-objects-snack-2.js
@@ -12,3 +12,17 @@ una copia di essi */
 copia dell'oggetto, però non copia oggetti/array annidati */
 
 /* dopo l`esecuzione di questo snippet, sono stati creati due oggetti: hamburger e secondBurger */
+
+//code Question 3
+
+/* In questo codice sono stati creati 3 oggetti: hamburger, secondBurger e thirdBurger, dato che è stato
+utilizzato "structuredClone" come metodo per copiare gli oggetti */
+
+//code Question 4
+
+/* il miglior metodo per clonare l`oggetto "chef" è utilizzare l'operatore "spread (...)", dato che l`oggetto non ha 
+array/oggetti annidati e contiene al suo interno una funzione che con i metodi JSON e structuredClone non verrebe clonato */
+
+/* il metodo migliore per clonare l`oggetto "restaurant" è lo "StructuredClone", dato che il metodo è in grado di copiare oggetti
+complessi e mantiene invariabile lo stato della proprietà "Date". In caso contrario, se usassimo il JSON.PARSE quella proprietà
+verrebbe convertita in una stringa */
