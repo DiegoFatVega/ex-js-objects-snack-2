9a5777063ba2cc4db0cc1f0116a5848f45f9a81d
DiegoFatVega <fatvegadiego@gmail.com>
2025-12-09 21:06:57 +0100

    add code snippets for questions 1 and 2 in ex-js-objects-snack-2.js

---
+++ ex-js-objects-snack-2.js
@@ -0,0 +1,14 @@
+//code Question 1
+
+/* il primo console.log stampa 'Double Cheese Burger' e il secondo stampa la stessa ConstantSourceNode,
+dato che gli oggetti sono dei reference type e non stiamo applicando dei metodi appropriati per creare
+una copia di essi */
+
+/* in quel codice non stiamo creando nessuna copia */
+
+//code Question 2
+
+/* il primo console.log stampa "cheese". Qui possiamo vedere che è stato usato l`operatore spread per creare una
+copia dell'oggetto, però non copia oggetti/array annidati */
+
+/* dopo l`esecuzione di questo snippet, sono stati creati due oggetti: hamburger e secondBurger */
