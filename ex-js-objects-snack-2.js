daea0e09cc6852bd572b14b3bce9394a23c5bcaf
DiegoFatVega <fatvegadiego@gmail.com>
2025-12-11 19:45:49 +0100

    correction of exercises in ex-js-objects-snack-2.js

--- ex-js-objects-snack-2.js
+++ ex-js-objects-snack-2.js
@@ -4,19 +4,20 @@
 dato che gli oggetti sono dei reference type e non stiamo applicando dei metodi appropriati per creare
 una copia di essi */
 
-/* in quel codice non stiamo creando nessuna copia */
+/* in quel codice non stiamo creando nessuna copia */ /* Correzione: 1 oggetto, la mia risposta si riferiva ad una copia, non all`oggetto */
 
 //code Question 2
 
 /* il primo console.log stampa "cheese". Qui possiamo vedere che è stato usato l`operatore spread per creare una
-copia dell'oggetto, però non copia oggetti/array annidati */
+copia dell'oggetto, però non copia oggetti/array annidati */ /*correzione: stampa "Salad", dato che dopo la duplicazione viene sostituito il primo elemento in ingredients*/
 
 /* dopo l`esecuzione di questo snippet, sono stati creati due oggetti: hamburger e secondBurger */
 
 //code Question 3
 
 /* In questo codice sono stati creati 3 oggetti: hamburger, secondBurger e thirdBurger, dato che è stato
-utilizzato "structuredClone" come metodo per copiare gli oggetti */
+utilizzato "structuredClone" come metodo per copiare gli oggetti */  /*correzione: 3 oggetti, uno hamburger, due maker e 3 restaurant, poi diventano 9 con secondburger e thirdburger*/
+
 
 //code Question 4
 
